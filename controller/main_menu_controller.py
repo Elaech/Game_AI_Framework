c4f357a592c5b0a8328e64019e05cb65c4c14a0f
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-15 15:55:25 +0200

    Update main_menu_controller.py

--- controller/main_menu_controller.py
+++ controller/main_menu_controller.py
@@ -1,9 +1,19 @@
 import main_controller
-
+import pygame
 """
 Main Menu controller that controls the interaction between the user and the application's main menu
 """
 
 
+def check_if_callable(mouse):
+    return false
+
 def start():
-    print("Main Menu")
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return None
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                check_if_callable(pygame.mouse.get_pos())
+

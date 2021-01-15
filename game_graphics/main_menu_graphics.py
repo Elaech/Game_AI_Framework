c7ba4ac9edd0e3376e7ead54ee4884cb35007c28
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2021-01-15 17:05:04 +0200

    Menu Button Presses

--- game_graphics/main_menu_graphics.py
+++ game_graphics/main_menu_graphics.py
@@ -2,7 +2,31 @@ import pygame.freetype
 import pygame
 from game_graphics import main_graphics as mg
 
+
+def is_play_button_pressed(x, y):
+    if mg.width * 2 / 5 < x < mg.width * 3 / 5 and mg.height * 2 / 16 < y < mg.height * 4 / 16:
+        return True
+    else:
+        return False
+
+
+def is_settings_button_pressed(x, y):
+    if mg.width * 2 / 5 < x < mg.width * 3 / 5 and mg.height * 6 / 16 < y < mg.height * 8 / 16:
+        return True
+    else:
+        return False
+
+
+def is_exit_button_pressed(x, y):
+    if mg.width * 2 / 5 < x < mg.width * 3 / 5 and mg.height * 10 / 16 < y < mg.height * 12 / 16:
+        return True
+    else:
+        return False
+
+
 def draw_main_menu():
     mg.window.fill(mg.color_scheme["background_color"])
-    mg.draw_button(mg.width*2/5, mg.height*2/10, mg.width/5, mg.height*2/10, "Play")
+    mg.draw_button(mg.width * 2 / 5, mg.height * 2 / 16, mg.width / 5, mg.height * 2 / 16, "Play")
+    mg.draw_button(mg.width * 2 / 5, mg.height * 6 / 16, mg.width / 5, mg.height * 2 / 16, "Settings")
+    mg.draw_button(mg.width * 2 / 5, mg.height * 10 / 16, mg.width / 5, mg.height * 2 / 16, "Exit")
     pygame.display.update()

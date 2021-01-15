fda047c7a4d3d91035ed0a117504c7300102eb98
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2021-01-15 17:10:54 +0200

    Quick Fix

--- game_graphics/main_menu_graphics.py
+++ game_graphics/main_menu_graphics.py
@@ -3,21 +3,27 @@ import pygame
 from game_graphics import main_graphics as mg
 
 
-def is_play_button_pressed(x, y):
+def is_play_button_pressed(mouse):
+    x = mouse[0]
+    y = mouse[1]
     if mg.width * 2 / 5 < x < mg.width * 3 / 5 and mg.height * 2 / 16 < y < mg.height * 4 / 16:
         return True
     else:
         return False
 
 
-def is_settings_button_pressed(x, y):
+def is_settings_button_pressed(mouse):
+    x = mouse[0]
+    y = mouse[1]
     if mg.width * 2 / 5 < x < mg.width * 3 / 5 and mg.height * 6 / 16 < y < mg.height * 8 / 16:
         return True
     else:
         return False
 
 
-def is_exit_button_pressed(x, y):
+def is_exit_button_pressed(mouse):
+    x = mouse[0]
+    y = mouse[1]
     if mg.width * 2 / 5 < x < mg.width * 3 / 5 and mg.height * 10 / 16 < y < mg.height * 12 / 16:
         return True
     else:

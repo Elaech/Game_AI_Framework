76217fffd05b56bc8417dce4ea721ca66073beb9
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-15 17:47:30 +0200

    Started Game Settings Controller

--- controller/game_settings_controller.py
+++ controller/game_settings_controller.py
@@ -1,5 +1,16 @@
 from controller import main_controller
+from game_graphics import settings_menu_graphics
 
 
 def start():
-    print("Game Settings")
+    clock = pygame.time.Clock()
+    settings_menu_graphics.draw_settings_menu()
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return None
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                controller = get_callable(pygame.mouse.get_pos())
+                if controller is not None:
+                    return controller

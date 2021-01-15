146e914923fb27d1dc206457a5d13c196c9d12bb
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-15 17:53:36 +0200

    Update game_settings_controller.py

--- controller/game_settings_controller.py
+++ controller/game_settings_controller.py
@@ -1,5 +1,26 @@
 from controller import main_controller
 from game_graphics import settings_menu_graphics
+import pygame
+import tkinter
+
+
+def clicked_on_back(mouse):
+    return False
+
+
+def clicked_on_board_width(mouse):
+    return False
+
+
+def clicked_on_board_height(mouse):
+    return False
+
+
+def clicked_on_alfabeta(mouse):
+    return False
+
+def clicked_on_simple_moves(mouse):
+    return False
 
 
 def start():
@@ -11,6 +32,4 @@ def start():
             if event.type == pygame.QUIT:
                 return None
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                controller = get_callable(pygame.mouse.get_pos())
-                if controller is not None:
-                    return controller
+                print("Text menu")

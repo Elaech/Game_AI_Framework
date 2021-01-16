fb643741a0160e8455357dc049277e89f6b39b4f
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2021-01-17 00:32:52 +0200

    Default Winning Method

--- game_logic/game_logic.py
+++ game_logic/game_logic.py
@@ -210,4 +210,19 @@ def default_winning_method():
     # True - HUMAN wins
     # False - AI wins
     # None - Nobody won
+    height = settings["board_height"]
+    width = settings["board_width"]
+    number_of_pieces = settings["number_of_pieces"]
+    piece_count = 0
+    for i in range(width):
+        if board[0][i] == 2:
+            piece_count += 1
+    if number_of_pieces == piece_count:
+        return True
+    piece_count = 0
+    for i in range(width):
+        if board[height - 1][i] == 1:
+            piece_count += 1
+    if number_of_pieces == piece_count:
+        return False
     return None

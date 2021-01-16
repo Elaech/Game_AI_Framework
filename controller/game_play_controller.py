b8e5f61ec3c67879e4a18d8d445068d589084ae2
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-16 22:59:05 +0200

    Update game_play_controller.py

--- controller/game_play_controller.py
+++ controller/game_play_controller.py
@@ -28,25 +28,25 @@ def start(settings):
     current_turn = game_logic.get_current_turn()
     playing_game = True
     while playing_game:
-        if game_logic.somebody_won() is not None:
-            if game_logic.somebody_won() is True:
-                HUMAN_won("HUMAN acomplished the objective")
-            else:
-                AI_won("AI acomplished the objective")
-        if not game_logic.there_are_possible_moves(current_turn):
-            if not game_logic.there_are_possible_moves(game_logic.get_other_turn()):
-                if game_logic.is_AI_turn():
-                    HUMAN_won("AI player has no moves")
-                else:
-                    AI_won("Human player has no moves")
-                playing_game = False
-                continue
-            else:
-                game_logic.change_turn()
+        # if game_logic.somebody_won() is not None:
+        #     if game_logic.somebody_won() is True:
+        #         HUMAN_won("HUMAN acomplished the objective")
+        #     else:
+        #         AI_won("AI acomplished the objective")
+        # if not game_logic.there_are_possible_moves(current_turn):
+        #     if not game_logic.there_are_possible_moves(game_logic.get_other_turn()):
+        #         if game_logic.is_AI_turn():
+        #             HUMAN_won("AI player has no moves")
+        #         else:
+        #             AI_won("Human player has no moves")
+        #         playing_game = False
+        #         continue
+        #     else:
+        #         game_logic.change_turn()
         if game_logic.is_AI_turn():
             AI_turn()
         else:
-            player_exits = HUMAN_turn()
+            player_exits = HUMAN_turn(settings)
             if player_exits:
                 break
         current_turn = game_logic.get_current_turn()
@@ -68,19 +68,57 @@ def calibrate_piece_settings(settings):
     return settings
 
 
-def AI_turn():
-    pass
+def draw_hover(line, column, changes):
+    hovers = []
+    for change in changes:
+        if game_logic.is_pos_within_bounds(column + change[1], line + change[0]) and not game_logic.is_occupied(
+                column + change[1], line + change[0]):
+            play_game_graphics.color_hover_cell(line + change[0], column + change[1])
+            hovers.append([line + change[0], column + change[1]])
+        elif game_logic.is_pos_within_bounds(column + change[1], line + change[0]) and change[1] == 0 \
+                and change[0] == 0:
+            play_game_graphics.color_hover_cell(line, column)
+            play_game_graphics.draw_player_piece(line, column, True)
+            hovers.append([line, column])
+    return hovers
 
 
-def HUMAN_turn():
+def delete_hover(list_of_positions, selected_piece):
+    for pos in list_of_positions:
+        play_game_graphics.clear_cell(pos[0], pos[1])
+        if pos[0] == selected_piece[0] and pos[1] == selected_piece[1]:
+            play_game_graphics.draw_player_piece(pos[0], pos[1], True)
+
+
+def HUMAN_turn(settings):
     clock = pygame.time.Clock()
+    selected_piece = None
+    last_hover = None
     while True:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return False
+                return True
             elif event.type == pygame.MOUSEBUTTONUP:
-                return False
+                if play_game_graphics.is_back_button_pressed(pygame.mouse.get_pos()):
+                    return True
+                line, column = play_game_graphics.get_board_click(pygame.mouse.get_pos())
+                if line > -1 and column > -1:
+                    if game_logic.is_my_piece(game_logic.PosTypes.HUMAN, column, line):
+                        if selected_piece is None:
+                            selected_piece = [line, column]
+                            last_hover = draw_hover(line, column, settings["simple_moves"])
+                        else:
+                            delete_hover(last_hover, selected_piece)
+                            selected_piece = [line, column]
+                            last_hover = draw_hover(line, column, settings["simple_moves"])
+                    elif game_logic.is_my_piece(game_logic.PosTypes.EMPTY, column, line) and selected_piece is not None:
+                        if game_logic.legal_move(selected_piece[1], selected_piece[0], column, line):
+                            game_logic.make_move(selected_piece[1], selected_piece[0], column, line)
+                            play_game_graphics.clear_cell(selected_piece[0], selected_piece[1])
+                            delete_hover(last_hover, selected_piece)
+                            play_game_graphics.draw_player_piece(line, column, True)
+                            return False
 
 
 def AI_won(message):
@@ -88,4 +126,8 @@ def AI_won(message):
 
 
 def HUMAN_won(message):
+    pass
+
+
+def AI_turn():
     pass

daf342539fe37d890b28eeb680bb920afab288cc
Dominte Florin <dominte.florin.iulian@gmail.com>
2021-01-17 02:35:48 +0200

    MinMax

--- game_logic/ai_logic.py
+++ game_logic/ai_logic.py
@@ -1,6 +1,7 @@
 import math
 from game_logic import game_logic
 
+
 settings = None
 score_heuristic = None
 win_heuristic = None
@@ -16,8 +17,33 @@ def init_ai_logic(options, score_method, winning_method):
 
 
 def get_next_move():
-    print(win_heuristic())
-    print(score_heuristic())
+    print(AI_minmax(game_logic.board))
+    initial_line, initial_column, after_line, after_column = AI_minmax(game_logic.board)
+    game_logic.make_move(initial_column, initial_line, after_column, after_line)
+
+
+
+def AI_minmax(board):
+    initial_line = 0
+    initial_column = 0
+    after_line = 0
+    after_column = 0
+
+    for i in range(0, settings["board_height"]):
+        for j in range(0, settings["board_width"]):
+            if board[i][j] == game_logic.PosTypes.AI:
+                for move in settings["simple_moves"]:
+                    max_value = 0
+                    if game_logic.legal_move(i, j, i - move[0], j - move[1]):
+                        gained_value = minmax(board, 2, True, score_heuristic, win_heuristic)
+                        if gained_value > max_value:
+                            max_value = gained_value
+                            initial_line = i
+                            initial_column = j
+                            after_line = i - move[0]
+                            after_column = j - move[1]
+
+    return initial_line, initial_column, after_line, after_column
 
 
 def minmax(board, depth, maximizing_player, score_heuristic, win_heuristic):

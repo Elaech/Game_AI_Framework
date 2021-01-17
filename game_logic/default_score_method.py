a8ea26b94f9aea53216b50615bd6f16aeb6209f0
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-17 16:41:12 +0200

    Update default_score_method.py

--- game_logic/default_score_method.py
+++ game_logic/default_score_method.py
@@ -1,4 +1,6 @@
 def score_method(height, width, board, enum):
+    # Returns an int or float
+    # Represents the value of a state for the AI
     human_score = 0
     AI_score = 0
     for i in range(height):

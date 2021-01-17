98393e5c85b3c9d6297a4c1e21620791ee6565fe
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2021-01-17 17:23:51 +0200

    Update ai_logic.py

--- game_logic/ai_logic.py
+++ game_logic/ai_logic.py
@@ -29,30 +29,30 @@ def AI_minmax(board):
     max_value = -math.inf
 
     shuffled_settings = copy.deepcopy(settings["simple_moves"])
-
+    ok_moves_queue = []
     for i in range(0, settings["board_height"]):
         for j in range(0, settings["board_width"]):
             if board[i][j] == game_logic.PosTypes.AI:
                 random.shuffle(shuffled_settings)
                 for move in shuffled_settings:
                     if game_logic.legal_move(i, j, i - move[0], j - move[1], game_logic.PosTypes.AI):
-                        gained_value = minmax(board, 2, True)
-                        if gained_value >= max_value:
+                        gained_value = minmax(board, 2, True, shuffled_settings)
+                        if gained_value > max_value:
                             print(gained_value, " ", max_value)
+                            del ok_moves_queue
+                            ok_moves_queue = [[i, j, i - move[0], j - move[1]]]
                             max_value = gained_value
-                            initial_line = i
-                            initial_column = j
-                            after_line = i - move[0]
-                            after_column = j - move[1]
-
-    return initial_line, initial_column, after_line, after_column
+                        elif gained_value == max_value:
+                            ok_moves_queue.append([i, j, i - move[0], j - move[1]])
+    return random.choice(ok_moves_queue)
 
 
-def minmax(board, depth, maximizing_player):
+def minmax(board, depth, maximizing_player, moves):
     if depth == 0:
         return score_heuristic(settings["board_height"], settings["board_width"], game_logic.board, game_logic.PosTypes)
 
-    win_state = win_heuristic(settings["board_height"], settings["board_width"], game_logic.board, settings["number_of_pieces"],
+    win_state = win_heuristic(settings["board_height"], settings["board_width"], game_logic.board,
+                              settings["number_of_pieces"],
                               game_logic.PosTypes)
 
     if win_state is True:
@@ -62,15 +62,17 @@ def minmax(board, depth, maximizing_player):
     elif win_state is None:
         pass
 
+    random.shuffle(moves)
+
     if maximizing_player:
         max_eval = -math.inf
         for i in range(0, settings["board_height"]):
             for j in range(0, settings["board_width"]):
                 if board[i][j] == game_logic.PosTypes.AI:
-                    for move in settings["simple_moves"]:
+                    for move in moves:
                         if game_logic.legal_move(i, j, i - move[0], j - move[1], game_logic.PosTypes.AI):
                             game_logic.make_move_AI(i, j, i - move[0], j - move[1])
-                            eval = minmax(board, depth - 1, False)
+                            eval = minmax(board, depth - 1, False, moves)
                             game_logic.make_move_AI(i - move[0], j - move[1], i, j)
                             max_eval = max(max_eval, eval)
 
@@ -81,10 +83,10 @@ def minmax(board, depth, maximizing_player):
         for i in range(0, settings["board_height"]):
             for j in range(0, settings["board_width"]):
                 if board[i][j] == game_logic.PosTypes.HUMAN:
-                    for move in settings["simple_moves"]:
+                    for move in moves:
                         if game_logic.legal_move(i, j, i + move[0], j + move[1], game_logic.PosTypes.HUMAN):
                             game_logic.make_move_AI(i, j, i + move[0], j + move[1])
-                            eval = minmax(board, depth - 1, True)
+                            eval = minmax(board, depth - 1, True, moves)
                             game_logic.make_move_AI(i + move[0], j + move[1], i, j)
                             min_eval = min(min_eval, eval)
         return min_eval
